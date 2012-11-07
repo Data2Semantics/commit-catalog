cd9cfc8419280aca6891c7e51ca84825fe1e3c4c
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-11-07 13:33:54 +0100

    Render stuff differently

--- models.py
+++ models.py
@@ -11,3 +11,53 @@ class Entry(models.Model):
     
     class Meta:
         app_label = 'catalog'
+
+
+#class Type(models.Model):
+#    name = models.CharField(max_length=100)
+#    
+#    def __unicode__(self):
+#        return self.name
+#    
+#class Reporter(models.Model):
+#    name = models.CharField(max_length=100)
+#    
+#    def __unicode__(self):
+#        return self.name
+#
+#class Project(models.Model):
+#    name = models.CharField(max_length=100)
+#    
+#    def __unicode__(self):
+#        return self.name
+#
+#    
+#class Domain(models.Model):
+#    name = models.CharField(max_length=100)
+#    
+#    def __unicode__(self):
+#        return self.name
+#
+#    
+#class Use(models.Model):
+#    name = models.CharField(max_length=100)
+#    
+#    def __unicode__(self):
+#        return self.name
+#
+#           
+#class VocabData(models.Model):
+#    timestamp = models.DateTimeField()
+#    name = models.CharField(max_length=100)
+#    url = models.CharField(max_length=100)
+#    
+#    types = models.ManyToManyField(Type)
+#    reporters = models.ManyToManyField(Reporter)
+#    projects = models.ManyToManyField(Project)
+#    domains = models.ManyToManyField(Domain)
+#    usage = models.ManyToManyField(Use)
+#    
+#    def __unicode__(self):
+#        return self.name
+
+    
